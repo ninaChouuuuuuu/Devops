fe8b3d8518536f8a6ba6c71abd11590286c94907
ninaChouuuuuuu <117676431+ninaChouuuuuuu@users.noreply.github.com>
2025-02-21 12:34:23 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+{
+  "name": "Devops",
+  "version": "1.0.0",
+  "description": "Un projet Node.js",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "jest"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "devDependencies": {
+    "jest": "^29.0.0"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  },
+  "author": "Nina",
+  "license": "MIT"
+}
+
